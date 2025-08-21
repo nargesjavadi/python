92306e9a4281c305cd04ff09974c66ceb45cd7db
nargesjavadi <nrgs5831@gmail.com>
2025-08-21 12:29:42 +0330

    exercise of session 2

---
+++ session2/exercise.py
@@ -0,0 +1,34 @@
+# exercise 1
+
+Midterm = "19"
+Final = "18.5"
+
+Midterm1 = int(Midterm)
+Final1 = float(Final)
+
+Sum = Midterm1 + Final1
+Avg = Sum/2
+
+print("sum = " , Sum)
+print("averrage = " , Avg)
+
+# exercise 2 
+
+Length = "5"
+Width = 3
+Radius = 2.5
+Pi = 3.14
+
+Length1= int(Length)
+
+RectanglePerimeter = (Length1 + Width) * 2
+RectangleArea = Length1 * Width
+
+CirclePerimeter = Radius * 2 * Pi
+CircleArea = (Radius ** 2) * Pi
+
+print("rectangle perimeter = " , RectanglePerimeter)
+print("rectangle area =  " , RectangleArea)
+
+print("circle perimeter = " , CirclePerimeter)
+print("circle area = " , CircleArea)
---
+++ session2/test.py
@@ -0,0 +1,2 @@
+a = int(input("enter your name"))
+print(a+3)
